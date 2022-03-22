ab53bea7d298cc8defe984346cff0171a981f9e1
Michelle Cheng <michellecheng.2017@gmail.com>
2022-03-21 22:33:58 -0400

    implemented fill color when cells are clicked

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Declare global variables
 let numRows = 0;
 let numCols = 0;
-let colorSelected;
+let colorSelected = document.getElementById("selectedColorId").value;
 
 // Add a row
 function addR() {
@@ -18,6 +18,18 @@ function addR() {
   const lastRow = table.lastChild;
   for (let i = 0; i < numCols; i++) {
     var td = document.createElement("td");
+    //call fillU on click
+    td.addEventListener(
+      "click",
+      function () {
+        var target = event.target;
+        if (colorSelected === "Red") target.style.backgroundColor = "red";
+        if (colorSelected === "Blue") target.style.backgroundColor = "blue";
+        if (colorSelected === "Green") target.style.backgroundColor = "green";
+        if (colorSelected === "Yellow") target.style.backgroundColor = "yellow";
+      },
+      false
+    );
     lastRow.appendChild(td);
   }
 }
@@ -38,6 +50,18 @@ function addC() {
   var rows = document.querySelectorAll("tr");
   for (let i = 0; i < numRows; i++) {
     var td = document.createElement("td");
+    //call fillU on click
+    td.addEventListener(
+      "click",
+      function () {
+        var target = event.target;
+        if (colorSelected === "Red") target.style.backgroundColor = "red";
+        if (colorSelected === "Blue") target.style.backgroundColor = "blue";
+        if (colorSelected === "Green") target.style.backgroundColor = "green";
+        if (colorSelected === "Yellow") target.style.backgroundColor = "yellow";
+      },
+      false
+    );
     rows[i].appendChild(td);
   }
 }
@@ -64,7 +88,9 @@ function removeC() {
 }
 
 // Set global variable for selected color
-function selectColor() {}
+function selectColor() {
+  colorSelected = document.getElementById("selectedColorId").value;
+}
 
 // Fill all uncolored cells
 function fillU() {}
